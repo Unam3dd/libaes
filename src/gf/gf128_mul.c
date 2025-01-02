3fbe296db0ea51d093cef183c913863a17ae3c20
Unam3dd <samtalespro@gmail.com>
2025-01-02 23:34:46 +0100

    📝 Update documentation

--- src/gf/gf128_mul.c
+++ src/gf/gf128_mul.c
@@ -2,31 +2,19 @@
 #include <immintrin.h>
 #include <smmintrin.h>
 #include <emmintrin.h>
+#include <wmmintrin.h>
 
 __m128i gf128_mul(__m128i a, __m128i b)
 {
-	__m256i poly = _mm256_set_epi64x(0, 0x1, 0, 0x87);
+	//const __m256i poly = _mm256_set_epi64x(0, 0x1, 0, 0x87);
 
-	__m256i res = _mm256_setzero_si256();
-	__m256i aa = _mm256_load_si256((__m256i*)&a);
-	__m256i bb = _mm256_setzero_si256();
+	__m128i low_low = _mm_clmulepi64_si128(a, b, 0x0);
+	__m128i high_high = _mm_clmulepi64_si128(a, b, 0x11);
+	__m128i mid_low = _mm_clmulepi64_si128(a, b, 0x01);
+	__m128i mid_high = _mm_clmulepi64_si128(a, b, 0x10);
 
-	_mm256_xor_si256(res, res);
+	_mm_xor_si128(low_low, mid_low);
+	_mm_xor_si128(high_high, mid_high);
 
-	__m256i tmp5 =  _mm256_clmulepi64_epi128(a, b, 0x0);
-	__m256i tmp4 = _mm256_clmulepi64_epi128(a, b, 0x10);
-	__m256i tmp3 = _mm256_clmulepi64_epi128(a, b, 0x01);
-	__m256i tmp2 = _mm256_clmulepi64_epi128(a, b, 0x11);
-
-	tmp5 = _mm256_xor_si256(tmp5, tmp3);
-	tmp2 = _mm256_xor_si256(tmp2, tmp4);
-
-	//res = _mm256_clmulepi64_si128(tmp5, poly, 0x10);
-
-	//tmp3 = _mm_shuffle_epi32(tmp5, 0x4e);
-
-	res = _mm256_xor_si256(res, tmp2);
-	res = _mm256_xor_si256(res, tmp3);
-
-	return (((__m128i*)&res)[0]);
+	return (_mm_setzero_si128());
 }
